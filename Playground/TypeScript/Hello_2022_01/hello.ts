f93788da7936c6d40bde4d69685343719ea03ffd
yunseokjeon <89689147+yunseokjeon@users.noreply.github.com>
2022-01-03 20:36:41 +0900

    0103

---
+++ Playground/TypeScript/Hello_2022_01/hello.ts
@@ -0,0 +1,7 @@
+console.log("Hello world!");
+
+function greet(person: string, date: Date) {
+    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+}
+
+greet("Maddison", new Date());
